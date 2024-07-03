d5021d1a969e3fef33773c540076617c53588420
DrR0X-glitch <zachcol08@gmail.com>
2024-07-04 03:54:15 +1000

    Remove flask and related utils

--- discord_bot/bot.py
+++ discord_bot/bot.py
@@ -4,14 +4,10 @@ import nextcord
 from nextcord import Interaction
 from nextcord.ext import commands
 from dotenv import load_dotenv
-from flask import Flask, request
 
-import utils
-
-load_dotenv()
+load_dotenv("../.env")
 
 bot = commands.Bot(command_prefix="!", intents=nextcord.Intents.all())
-app = Flask(__name__)
 
 
 @bot.event
@@ -26,31 +22,7 @@ async def test(interaction: Interaction):
     await interaction.response.send_message("Bot is currently connected! :smile:")
 
 
-@app.route("/github-webhook", methods=["POST"])
-def github_webhook():
-    data = request.json
-    message = utils.format_request_to_message(data)
-    channel = bot.get_channel(1192948772888641566)
-    channel.send(message)
-
-    return "Webhook received successfully!", 200
-
-
 bot.load_extension("get_help_cog")  # Load Get Help
 bot.load_extension("help_monitor_cog")  # Load Help Monitor
 
-
-def run_bot():
-    bot.run(os.getenv("DISCORD_BOT_TOKEN"))
-
-
-def run_flask_app():
-    app.run(host="0.0.0.0", debug=True)
-
-
-if __name__ == "__main__":
-    with ThreadPoolExecutor(max_workers=2) as executor:
-        executor.submit(run_bot)
-        executor.submit(run_flask_app)
-    while True:
-        pass
+bot.run(os.getenv("DISCORD_BOT_TOKEN"))
