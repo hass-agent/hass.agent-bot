db6a29b3e3c2486de5e5a445b765ed7c6fd4468f
DrR0X-glitch <zachcol08@gmail.com>
2024-07-04 10:36:47 +1000

    Add delay to allow new threads to propogate before replying

--- discord_bot/get_help_cog.py
+++ discord_bot/get_help_cog.py
@@ -1,5 +1,7 @@
+import asyncio
 import nextcord
 from nextcord.ext import commands
+
 
 class GetHelpCog(commands.Cog):
     def __init__(self, bot):
@@ -17,7 +19,7 @@ class GetHelpCog(commands.Cog):
             mark_solution_embed = nextcord.Embed(
                 title="Help us Help Others!",
                 description="To help others find answers, you can mark your question as solved via `Right click solution message -> Apps -> ✅ Mark Solution`",
-                color=0x000000
+                color=0x000000,
             )
             mark_solution_embed.set_image(url="attachment://mark_solution.png")
 
@@ -25,7 +27,7 @@ class GetHelpCog(commands.Cog):
             tags_embed = nextcord.Embed(
                 title="Help us Help You!",
                 description="Please don't delete messages or posts because it makes it impossible to understand what happened. If you don't want your messages to be seen then don't post here.",
-                color=0x000000
+                color=0x000000,
             )
 
             users_to_ping = set()
@@ -42,7 +44,7 @@ class GetHelpCog(commands.Cog):
             if "documentation" in tags:
                 users_to_ping.add(638245963240046592)
                 tags_embed.description += "\n\n**Please provide us with a direct link to the documentation.**\n> Please use permalinks to the section in the docs you are referencing. A guide for this is available [here](<https://www.hass-agent.io/latest/contributing/reporting-issues/#permalinks>)"
-            
+
             if "bug" in tags:
                 tags_embed.description += "\n\n**Make sure to provide detailed reports of bugs.**\n> Please include error log uploads and info about the bug."
 
@@ -50,14 +52,21 @@ class GetHelpCog(commands.Cog):
                 users_to_ping.add(638245963240046592)
 
             # Send Embeds
-            await thread.send(embed=mark_solution_embed, file=nextcord.File("mark_solution.png"))
-            await thread.send(embed=tags_embed)
+            await asyncio.sleep(5)
+            try:
+                await thread.send(
+                    embed=mark_solution_embed, file=nextcord.File("mark_solution.png")
+                )
+                await thread.send(embed=tags_embed)
 
-            # Mention users
-            for user_id in users_to_ping:
-                user = self.bot.get_user(user_id)
-                if user:
-                    await thread.send(user.mention)
+                # Mention users
+                for user_id in users_to_ping:
+                    user = self.bot.get_user(user_id)
+                    if user:
+                        await thread.send(user.mention)
+            except nextcord.HTTPException as e:
+                print(f"Failed to send message to thread: {e}")
+
 
 def setup(bot):
     bot.add_cog(GetHelpCog(bot))
